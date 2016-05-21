34e660c8a17b34fe9f2c3c1d6758b6bea61d4d9d
David Beers <dbeers@gmail.com>
2016-05-21 15:54:09 -0600

    fix path to environment local.js

--- data/local.js
+++ data/local.js
@@ -1,5 +1,5 @@
 // load the local app config based on the environment
 
-module.exports = require('./environments/' + (process.env.ENVIRONMENT || 'development') + '/local.js');
+module.exports = require('../environments/' + (process.env.ENVIRONMENT || 'development') + '/local.js');
 
 
